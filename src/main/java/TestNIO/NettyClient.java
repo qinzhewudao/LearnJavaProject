91d8a999edf91068bb1a651566fa66e4c9fffdb6
sheyang <yang.she@ele.me>
2019-10-14 19:02:53 +0800

    netty

---
+++ src/main/java/TestNIO/NettyClient.java
@@ -0,0 +1,40 @@
+package TestNIO;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.nio.channels.FileChannel;
+import java.util.Date;
+
+/**
+ * @author sheyang
+ * @description
+ * @date 2019/10/11
+ * @time 上午9:55
+ */
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        while (true) {
+            channel.writeAndFlush(new Date() + ": hello world!");
+            Thread.sleep(2000);
+        }
+    }
+}
